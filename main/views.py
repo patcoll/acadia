008beb8fb2eace8edc801aca0aae6250aa6f8bda
burned.com <burned.com@2fa1e8b3-ee55-0410-9b5f-f024115c36bd>
2009-05-04 22:21:48 +0000

    comment out unneeded
    git-svn-id: svn+ssh://burned.com/home/63904/data/svn/cms/trunk@12 2fa1e8b3-ee55-0410-9b5f-f024115c36bd

--- main/views.py
+++ main/views.py
@@ -5,6 +5,7 @@ from cms.main.models import Page
 
 # Function for displaying a page.
 def page_display(request, page_id, rest_of_url=''):
-    print page_id
+    # print page_id
     page = get_object_or_404(Page, pk=page_id)
+    # print page.template.name
     return render_to_response('site/%s.html' % page.template.name, locals())
