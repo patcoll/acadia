c17eb1d5d84cb7019f294d8de14c8719d654708d
Pat Collins <pat@walltowall.com>
2009-09-23 17:45:57 -0400

    fixed spacing

--- apps/news/urls.py
+++ apps/news/urls.py
@@ -3,8 +3,8 @@ from django.conf import settings
 from views import *
 
 urlpatterns = patterns('',
-  (r'^$', post_index),
-  (r'^(\d+)$', post_view),
-  (r'^categories/$', category_index),
-  (r'^categories/(\d+)$', category_view),
+    (r'^$', post_index),
+    (r'^(\d+)$', post_view),
+    (r'^categories/$', category_index),
+    (r'^categories/(\d+)$', category_view),
 )
--- apps/news/views.py
+++ apps/news/views.py
@@ -5,13 +5,13 @@ from django.shortcuts import render_to_response, get_object_or_404
 
 # Function for displaying a page.
 def post_index(request):
-  return HttpResponse('posts')
+    return HttpResponse('posts')
   
 def post_view(request, post_id):
-  return HttpResponse('post: %s' % post_id)
+    return HttpResponse('post: %s' % post_id)
   
 def category_index(request):
-  return HttpResponse('categories')
+    return HttpResponse('categories')
 
 def category_view(request, category_id):
-  return HttpResponse('category: %s' % category_id)
+    return HttpResponse('category: %s' % category_id)
